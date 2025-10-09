efac5fa64439b0a30229a807f2b438014fae2de1
infinite-kube <kcarrow@infinite.com>
2025-10-09 11:50:45 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
+const express = require('express');
 const _ = require('lodash');
 const axios = require('axios');
-const express = require('express');
 const dayjs = require('dayjs');
 
 const app = express();
+
 app.get('/', (_req, res) => {
   res.json({
-    msg: 'Hello from the Outdated-Libs demo!',
+    msg: 'Outdated-libs demo',
     lodash: _.VERSION,
+    axios: require('axios/package.json').version,
     day: dayjs().format()
   });
 });
-module.exports = app;
 
 if (require.main === module) {
   const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Demo on http://localhost:${port}`));
+  app.listen(port, () =>
+    console.log(`Outdated-libs demo listening on http://localhost:${port}`)
+  );
 }
+
+module.exports = app;
